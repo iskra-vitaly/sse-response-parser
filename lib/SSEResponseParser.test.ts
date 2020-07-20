55cfbd0f81b1bff96416dab5084e6dae62e34d16
Iskra Vitaly <iskra.vitaly@gmail.com>
2020-07-20 23:31:48 +0300

    Add and configure jest

---
+++ lib/SSEResponseParser.test.ts
@@ -0,0 +1,3 @@
+it('should return true', function () {
+    expect(2).toEqual(2)
+}); 
--- lib/SSEResponseParser.ts
+++ lib/SSEResponseParser.ts
@@ -1,3 +1,13 @@
 export default class SSEResponseParser {
+    constructor() {
+    }
+}
 
+class SSEParserState {
+    constructor() {
+    }
+
+    nextState(input: Uint8Array): SSEParserState {
+        return this;
+    }
 }
