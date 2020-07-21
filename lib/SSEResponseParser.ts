6318937c5047cbb005a0b3eb2791290326836d90
Iskra Vitaly <iskra.vitaly@gmail.com>
2020-07-21 23:51:50 +0300

    Slight refactoring

--- lib/SSEResponseParser.ts
+++ lib/SSEResponseParser.ts
@@ -151,6 +151,8 @@ export default class SSEResponseParser {
     }
 }
 
+type BufferCollectResult = Generator<ParserOutput, { endingChar: number, buffer: string, pos: number }, ParserInput>;
+
 function* fieldStart(ctx: ParserCtx): ParserStateResult {
     const {chunk, pos} = yield {};
     const char = chunk[pos]
@@ -170,39 +172,16 @@ function* fieldStart(ctx: ParserCtx): ParserStateResult {
 }
 
 function* fieldName(ctx: ParserCtx): ParserStateResult {
-    let {chunk, pos} = yield {};
-    let fieldName = '';
-    let nameEndChar = 0;
-    // Request new chunks until fieldName ending character encountered
-    while (nameEndChar === 0) {
-        const remainingChunk = chunk.subarray(pos);
-        const nameEndIndex = remainingChunk.findIndex(notNameChar);
-        let nameChunk = null;
-        if (nameEndIndex >= 0) {
-            nameChunk = remainingChunk.subarray(0, nameEndIndex);
-            nameEndChar = remainingChunk[nameEndIndex];
-            pos += nameEndIndex;
-        } else {
-            // Need more chunks to read the name
-            const nextChunk = yield {pos: chunk.byteLength};
-            chunk = nextChunk.chunk;
-            pos = nextChunk.pos;
-            nameChunk = remainingChunk;
-        }
-        fieldName += ctx.decoder.decode(nameChunk, {stream: true});
-    }
-    pos++; // Skip the name ending character
-    // Flush the decoder
-    fieldName += ctx.decoder.decode();
-    ctx.decoder = new TextDecoder();
+    const {endingChar, buffer, pos} = yield* collectUntilChar(ctx, notNameChar, false);
+    const fieldName = buffer;
     // Field name without field value is a special case according to the spec
-    if (nameEndChar === LF || nameEndChar === CR) {
+    if (endingChar === LF || endingChar === CR) {
         /*
           according to spec here https://html.spec.whatwg.org/multipage/server-sent-events.html#parsing-an-event-stream
           the value of the feild with the name ending with a line-end should be considered empty string
          */
         ctx.message[fieldName] = '';
-        return {pos, nextState: skipLFAfterCR(nameEndChar, fieldStart)};
+        return {pos, nextState: skipLFAfterCR(endingChar, fieldStart)};
     } else {
         // Got field name followed by a colon. Now parse the field value.
         return {pos, nextState: fieldValue(fieldName)};
@@ -211,41 +190,15 @@ function* fieldName(ctx: ParserCtx): ParserStateResult {
 
 function fieldValue(fieldName: string): ParserState {
     return function* (ctx: ParserCtx): ParserStateResult {
-        let {chunk, pos} = yield {};
-        let fieldValue = '';
-        let valueEndChar = 0;
-        // Request new chunks until line-end character encountered
-        while (valueEndChar === 0) {
-            const remainingChunk = chunk.subarray(pos);
-            const valueEndIndex = remainingChunk.findIndex(lineEndChar);
-            let valueChunk = null;
-            if (valueEndIndex >= 0) {
-                valueChunk = remainingChunk.subarray(0, valueEndIndex);
-                valueEndChar = remainingChunk[valueEndIndex];
-                pos += valueEndIndex;
-            } else {
-                // Need more chunks to read the value
-                const nextChunk = yield {pos: chunk.byteLength};
-                chunk = nextChunk.chunk;
-                pos = nextChunk.pos;
-                valueChunk = remainingChunk;
-            }
-            // Skip leading space
-            if (fieldValue === '' && valueChunk.byteLength > 0 && valueChunk[0] === SPACE) {
-                valueChunk = valueChunk.subarray(1);
-            }
-            fieldValue += ctx.decoder.decode(valueChunk, {stream: true});
-        }
-        pos++; // Skip the line-end character
-        // Flush the decoder
-        fieldValue += ctx.decoder.decode();
-        ctx.decoder = new TextDecoder();
+        const {endingChar, buffer, pos} = yield* collectUntilChar(ctx, notNameChar, true);
+        let fieldValue = buffer;
+        // Several data values should be concatenated according to a spec
         if (fieldName === 'data') {
             const previous = ctx.message[fieldName] || '';
             fieldValue = previous + fieldValue + '\n';
         }
         ctx.message[fieldName] = fieldValue;
-        return {pos, nextState: skipLFAfterCR(valueEndChar as LINE_END, fieldStart)};
+        return {pos, nextState: skipLFAfterCR(endingChar as LINE_END, fieldStart)};
     }
 }
 
@@ -283,4 +236,40 @@ function* skipComment(): ParserStateResult {
         chunk = nextChunk.chunk;
         pos = nextChunk.pos;
     }
+}
+
+function* collectUntilChar(ctx: ParserCtx,
+                           predicate: (ch: number) => boolean,
+                           skipLeadingSpace: boolean): BufferCollectResult {
+    let {chunk, pos} = yield {};
+    let buffer = '';
+    let endingChar = 0;
+    // Request new chunks until character of interest is encountered
+    while (endingChar === 0) {
+        const remainingChunk = chunk.subarray(pos);
+        const endingIndex = remainingChunk.findIndex(predicate);
+        let bytesToAdd = null;
+        if (endingIndex >= 0) {
+            bytesToAdd = remainingChunk.subarray(0, endingIndex);
+            endingChar = remainingChunk[endingIndex];
+            pos += endingIndex;
+        } else {
+            // Need more chunks to read the value
+            const nextChunk = yield {pos: chunk.byteLength};
+            chunk = nextChunk.chunk;
+            pos = nextChunk.pos;
+            bytesToAdd = remainingChunk;
+        }
+        if (skipLeadingSpace) {
+            if (buffer === '' && bytesToAdd.byteLength > 0 && bytesToAdd[0] === SPACE) {
+                bytesToAdd = bytesToAdd.subarray(1);
+            }
+        }
+        buffer += ctx.decoder.decode(bytesToAdd, {stream: true});
+    }
+    pos++; // Skip the ending character
+    // Flush the decoder
+    buffer += ctx.decoder.decode();
+    ctx.decoder = new TextDecoder();
+    return {endingChar, buffer, pos};
 }
